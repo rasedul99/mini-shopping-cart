53add3d0fbaa36dff2df25affed3205c11b88f7f
rasedul99 <rasedul2199@gmail.com>
2022-02-16 22:27:59 +0600

    calculate phone cost

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber(isIncreasing) {
-  const caseInput = document.getElementById("input-value");
+function updateProductNumber(product, isIncreasing, price) {
+  const caseInput = document.getElementById(product + "-number");
   let caseNumber = caseInput.value;
   if (isIncreasing == true) {
     caseNumber = parseInt(caseNumber) + 1;
@@ -10,14 +10,22 @@ function updateCaseNumber(isIncreasing) {
 
   // update caseTotal
 
-  const caseTotal = document.getElementById("case-total");
-  caseTotal.innerText = caseNumber * 59;
+  const caseTotal = document.getElementById(product + "-total");
+  caseTotal.innerText = caseNumber * price;
 }
-
+//  phone case  increase
 document.getElementById("plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateProductNumber("case", true, 59);
+});
+// phone case decrease
+document.getElementById("minus").addEventListener("click", function () {
+  updateProductNumber("case", false, 59);
 });
 
-document.getElementById("minus").addEventListener("click", function () {
-  updateCaseNumber(false);
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateProductNumber("phone", true, 1235);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateProductNumber("phone", false, 1235);
 });
