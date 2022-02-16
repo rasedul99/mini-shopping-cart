574f41e68e4ff24b8ebcae7964fc3920b3fdd20d
rasedul99 <rasedul2199@gmail.com>
2022-02-17 00:56:58 +0600

    subtotal,tax,total price calculated

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 function updateProductNumber(product, isIncreasing, price) {
   const caseInput = document.getElementById(product + "-number");
-  let caseNumber = caseInput.value;
+  let productNumber = caseInput.value;
   if (isIncreasing == true) {
-    caseNumber = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseNumber = parseInt(caseNumber) - 1;
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
+  caseInput.value = productNumber;
 
-  // update caseTotal
+  // update productTotal
 
-  const caseTotal = document.getElementById(product + "-total");
-  caseTotal.innerText = caseNumber * price;
+  const productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
+
+  const subTotal = calculateTotal();
+  console.log(subTotal);
+}
+
+function getInput(product) {
+  const productInput = document.getElementById(product + "-number");
+  const productNumber = parseInt(productInput.value);
+  console.log(productNumber);
+  return productNumber;
+}
+function calculateTotal() {
+  const phoneTotal = getInput("phone") * 1235;
+  const caseTotal = getInput("case") * 59;
+  const subTotal = phoneTotal + caseTotal;
+  const tax = subTotal / 10;
+  const totalPrice = subTotal + tax;
+  document.getElementById("tax-amount").innerText = tax;
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("total-price").innerText = totalPrice;
 }
 //  phone case  increase
 document.getElementById("plus").addEventListener("click", function () {
@@ -21,11 +41,11 @@ document.getElementById("plus").addEventListener("click", function () {
 document.getElementById("minus").addEventListener("click", function () {
   updateProductNumber("case", false, 59);
 });
-
+// phone Number increase
 document.getElementById("phone-plus").addEventListener("click", function () {
   updateProductNumber("phone", true, 1235);
 });
-
+// phone number decrease
 document.getElementById("phone-minus").addEventListener("click", function () {
   updateProductNumber("phone", false, 1235);
 });
